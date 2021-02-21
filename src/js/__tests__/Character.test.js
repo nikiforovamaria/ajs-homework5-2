f0775154b1bbaa8c619b504872ae392d95b083a7
Maria Nikiforova <nikiforovamary@gmail.com>
2021-02-21 12:53:00 +0600

    tests fixed

--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -7,15 +7,19 @@ test('should create bowman character', () => {
 });
 
 test('should give an error about name length', () => {
-  const bowman = new Character('M', 'Bowman');
   const result = 'Длина имени должна быть минимум 2 символа и максимум 10 символов';
-  expect(bowman).toThrow(result);
+  expect(() => {
+    const bowman = new Character('M', 'Bowman');
+    return bowman;
+  }).toThrow(result);
 });
 
 test('should give an error about type', () => {
-  const bowman = new Character('Mike', 'Bowmann');
   const result = 'Тип должен быть один из перечисленных вариантов: Bowman, Swordsman, Magician, Daemon, Undead, Zombie';
-  expect(bowman).toThrow(result);
+  expect(() => {
+    const bowman = new Character('Mike', 'Bowmann');
+    return bowman;
+  }).toThrow(result);
 });
 
 test('should increase level', () => {
@@ -35,7 +39,9 @@ test('should take damage', () => {
 test('should give an error from levelUp', () => {
   const bowman = new Character('Mike', 'Bowman', 0, 1, 25, 25);
   const result = 'Нельзя повысить левел умершего';
-  expect(bowman.levelUp()).toThrow(result);
+  expect(() => {
+    bowman.levelUp();
+  }).toThrow(result);
 });
 
 test('should not take damage', () => {
